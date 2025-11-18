8f6302cf47d6b35b24ba426986ee161c9466416b
juancarlospizarro <jcpizarroalonso@gmail.com>
2025-11-18 18:07:21 +0100

    Cambio en la ruta base.

--- football_stats_app/settings.py
+++ football_stats_app/settings.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 

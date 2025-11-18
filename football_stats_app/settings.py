42f6b4c963e54b2163f4e7f1017a83ee1236107c
juancarlospizarro <jcpizarroalonso@gmail.com>
2025-11-18 18:15:24 +0100

    Cambio en la ruta raiz de las vistas.

--- football_stats_app/settings.py
+++ football_stats_app/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'football_stats_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'football_stats_app' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

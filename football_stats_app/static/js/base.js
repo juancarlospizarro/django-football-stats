d59fec6755f74159a075bf1e3261893055ab2287
juancarlospizarro <jcpizarroalonso@gmail.com>
2025-12-08 09:05:15 +0100

    Pequeños cambios en los archivos de vistas base.

---
+++ football_stats_app/static/js/base.js
@@ -0,0 +1,21 @@
+const html = document.documentElement;
+    const button = document.getElementById("themeToggle");
+
+    // Cargar tema guardado
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+        html.setAttribute("data-bs-theme", savedTheme);
+        updateButton(savedTheme);
+    }
+
+    button.addEventListener("click", () => {
+        const newTheme = html.getAttribute("data-bs-theme") === "light" ? "dark" : "light";
+        
+        html.setAttribute("data-bs-theme", newTheme);
+        localStorage.setItem("theme", newTheme);
+        updateButton(newTheme);
+    });
+
+    function updateButton(theme) {
+        button.innerHTML = theme === "dark" ? "☀️ Modo claro" : "🌙 Modo oscuro";
+    }

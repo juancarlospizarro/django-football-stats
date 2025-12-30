519770aa024ab000eb9b912442b37aad2f5f2583
juancarlospizarro <jcpizarroalonso@gmail.com>
2025-12-30 14:03:45 +0100

    Cambio en la definición del host de la base de datos en settings.py

--- football_stats_app/settings.py
+++ football_stats_app/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': os.getenv("DATABASE_NAME"),
         'USER': os.getenv("DATABASE_USER"),
         'PASSWORD': os.getenv("DATABASE_PASSWORD"),
-        'HOST': os.getenv("HOST"),
+        'HOST': os.getenv("DATABASE_HOST"),
         'PORT': '5432',
     }
 }
